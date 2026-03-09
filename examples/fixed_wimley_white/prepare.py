c58fd7229273b5c56cf46a7a01f35d46530370a6
gitesei <giu.tesei@gmail.com>
2026-03-09 10:58:56 +0100

    Add example ene calc fixed pos

---
+++ examples/fixed_wimley_white/prepare.py
@@ -0,0 +1,102 @@
+import os
+from calvados.cfg import Config, Job, Components
+import subprocess
+import numpy as np
+import pandas as pd
+from argparse import ArgumentParser
+from Bio import SeqIO
+
+parser = ArgumentParser()
+parser.add_argument('--name',nargs='?',required=True,type=str)
+
+args = parser.parse_args()
+
+# Dictionary to define starting APL (from lipid parametrization)
+eq_apl_dict = {
+    'DMPC': 0.57742,
+    'DPPC': 0.519,
+    'DOPC': 0.655,
+    'POPC': 0.64,
+}
+
+cwd = os.getcwd()
+N_save = int(5e4)
+N_frames = 1100
+Lx = 25
+Ly = Lx
+Lz = 60
+area_per_lipid = eq_apl_dict['POPC']
+N_lipids = int(np.ceil(Lx*Ly/area_per_lipid)*2)
+
+residues_file = f'{cwd}/input/residues.csv'
+
+sysname = args.name
+charge_termini = 'C' if sysname[2] in ['R','K'] else 'None'
+
+config = Config(
+  # GENERAL
+  sysname = sysname, # name of simulation system
+  box = [Lx, Ly, Lz], # nm
+  temp = 297.15,
+  ionic = 0.15, # molar
+  pH = 7,
+  topol = 'shift_ref_bead',
+  report_potential_energy = True,
+  slab_outer = 15,
+  bilayer_eq = True,
+  friction = 0.01,
+  pressure_coupling = True,
+  pressure = [0,0,0],
+
+  # RUNTIME SETTINGS
+  wfreq = N_save, # dcd writing frequency, 1 = 10fs
+  logfreq = N_save//5,
+  steps = N_frames*N_save, # number of simulation steps
+  steps_eq = 20*N_save,
+  runtime = 0, # overwrites 'steps' keyword if > 0
+  platform = 'CUDA', # 'CUDA' or 'OpenCL'
+  restart = 'checkpoint',
+  frestart = 'restart.chk',
+  verbose = True,
+)
+
+# PATH
+path = f'{cwd}/{sysname}'
+output_path = 'data'
+subprocess.run(f'mkdir -p {path}',shell=True)
+subprocess.run(f'mkdir -p {output_path}',shell=True)
+
+
+
+analyses = f"""
+from calvados.analysis import SlabAnalysis, calc_bilayer_prop
+
+slab = SlabAnalysis(name="{sysname:s}", input_path="{path:s}",
+                    output_path="{output_path:s}", ref_name="bilayer", 
+                    ref_chains = (1,{int(N_lipids):d}),
+                    client_names = ["protein"],
+                    client_chain_list = [(0,0)],
+                    verbose=True)
+
+slab.center(start=100, center_target='ref')
+slab.calc_profiles()
+slab.calc_concentrations()
+calc_bilayer_prop(path="{path:s}",sysname="{sysname:s}",output_path="{output_path:s}")
+"""
+
+config.write(path,name='config.yaml',analyses=analyses)
+
+components = Components(
+  # Defaults
+  molecule_type = 'protein',
+  nmol = 1, # number of molecules
+  restraint = False, # apply restraints
+  charge_termini = charge_termini, # charge N or C or both
+
+  # INPUT
+  ffasta = f'{cwd}/input/fastalib.fasta', # input fasta file
+  fresidues = residues_file, # residue definitions
+)
+components.add(name='POPC', molecule_type='lipid', nmol=int(N_lipids))
+components.add(name=args.name, molecule_type='protein', nmol=1, ref_bead=2, pos_bead=[0,0,2.5], fixed_bead=True)
+components.write(path,name='components.yaml')
